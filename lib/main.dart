0c6885d1bba2e88a04ab2b65f3906a83b0f892e2
mohali9 <mohali.ilkom@gmail.com>
2019-10-24 08:57:33 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:crud_lagi/api/api_service.dart';
+import 'package:crud_lagi/home/create.dart';
+import 'package:crud_lagi/home/homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.orange,
+        accentColor: Colors.orangeAccent,
+      ),
+      home: Scaffold(
+        key: _scaffoldState,
+        appBar: AppBar(
+          title: Text(
+            "Flutter CRUD API",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          actions: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  _scaffoldState.currentContext,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                    return FormAddScreen();
+                  }),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
